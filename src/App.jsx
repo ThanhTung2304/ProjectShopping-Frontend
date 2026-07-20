e831abd0840e35a405b5ed143af32a69cb8ab9fb
ThanhTung2304 <thanhtung232004@gmail.com>
2026-07-21 00:14:05 +0700

    Embedding chat

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import AppRouter from "./router";
 import ChatWidget from "./components/ChatWidget/ChatWidget";
+import { useLocation } from "react-router-dom";
 
+function ChatWidgetGate() {
+  const location = useLocation();
+  const isAdminRoute = location.pathname.startsWith("/admin");
+
+  if (isAdminRoute) return null;
+
+  return <ChatWidget />;
+}
 function App() {
   return (
     <>
       <AppRouter />
-      <ChatWidget />
+      <ChatWidgetGate />
     </>
   );
 }
