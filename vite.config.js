403856d5525d899704e6dfb76bd5253a3cc652b2
ThanhTung2304 <thanhtung232004@gmail.com>
2026-07-25 21:19:43 +0700

    fix vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    global: 'globalThis', // Giả lập biến "global" cho các thư viện gốc Node.js như sockjs-client
+  },
   server: {
     proxy: {
       '/api': {
